92dd1cead513a45e08d12c0dcf623f9a1158e724
adujoseph <iamjosephadu@gmail.com>
2019-02-01 00:28:31 +0100

    Initial Commit

---
+++ TweetGate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TweetGate
+//
+//  Created by MAC on 01/02/2019.
+//  Copyright © 2019 MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
